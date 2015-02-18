2dd002641a383f3ed3ed68b26dc29dccfd10633f
Shrayas <shrayasr@gmail.com>
2015-02-19 01:11:03 +0530

    Why so many `let`s ? Refactored!

--- src/numeronjm/core.clj
+++ src/numeronjm/core.clj
@@ -2,17 +2,13 @@
 
 (defn make-numeronym
   [input]
-  (let [cnt (count input)]
+  (let [cnt (count input)
+        start (first input)
+        end (last input)
+        nums (- (count input) 2)]
     (cond (= cnt 0) ""
-          (> cnt 2)
-            (let [start (first input)
-                  end   (last input)
-                  nums (- (count input) 2)]
-              (str start nums end))
-          (= cnt 2)
-            (let [start (first input)
-                  end (last input)]
-              (str start end)))))
+          (> cnt 2) (str start nums end)
+          (= cnt 2) (str start end))))
 
 (defn -main
   "main function"
