8f838e2323ad45daedaacdcc29b9683416a133a5
Shrayas <shrayasr@gmail.com>
2015-02-20 17:47:14 +0530

    Simple first cut web version of numeronjm

--- src/numeronjm/core.clj
+++ src/numeronjm/core.clj
@@ -3,20 +3,24 @@
             [compojure.route :as route]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
 
-(defroutes app-routes
-  (GET "/" [] "hello world")
-  (route/not-found "not found"))
-
 (defn make-numeronym
-  [input]
-  (let [cnt (count input)
-        start (first input)
-        end (last input)
-        nums (- (count input) 2)]
+  [request]
+  (let [word (get-in request [:params :word])
+        cnt (count word)
+        start (first word)
+        end (last word)
+        nums (- (count word) 2)]
     (cond (= cnt 0) ""
           (> cnt 2) (str start nums end)
           (= cnt 2) (str start end))))
 
+(defroutes app-routes
+  (GET "/"                  [] "run <pre>/numeronym/&lt;word&gt;</pre> for
+                               generating a numeronym")
+  (GET "/numeronym/:word"   []  (fn [request]
+                                  (let [numeronym (make-numeronym request)]
+                                    (str "<pre>" numeronym "</pre>"))))
+  (route/not-found "not found"))
 
 (def app
   (wrap-defaults app-routes site-defaults))
